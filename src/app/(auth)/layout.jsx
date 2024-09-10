948656a316317a9373f19c83bf4b062d0b60b24b
Ahmed Shoaib <ahmedeldomiaty0@gmail.com>
2024-09-10 10:38:25 +0300

    level2 lesson12

--- src/app/(auth)/layout.jsx
+++ src/app/(auth)/layout.jsx
@@ -4,7 +4,8 @@ import  '../globals.css';
 import  './auth.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import Footer from "componatnt/footer/footer";
-
+import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 
 const inter = Inter({
   subsets: ['latin'],
@@ -29,6 +30,18 @@ export default function RootLayout({ children }) {
     alignItems: "center",
       }}
       className={` auth text-center text-bg-dark ${inter.className}`}>
+        <ToastContainer
+position="top-right"
+autoClose={5000}
+hideProgressBar={false}
+newestOnTop={false}
+closeOnClick
+rtl={false}
+pauseOnFocusLoss
+draggable
+pauseOnHover
+theme="light"
+/>
       {children}
       <Footer/>
       </body>
--- src/app/(auth)/register/RegisterForm.jsx
+++ src/app/(auth)/register/RegisterForm.jsx
@@ -1,6 +1,7 @@
 "use client";
 import { useRouter } from "next/navigation";
 import React, { useState } from "react";
+import { toast } from "react-toastify";
 
 const RegisterForm = () => {
   const [Name, setName] = useState(null);
@@ -21,6 +22,7 @@ const RegisterForm = () => {
         //inputs empty
         if (!Email || !Name || !Password) {
           setError("All Input Must be Filled");
+          toast.error("All Input Must be Filled")
           setLoading(false)
           return;
         }
@@ -38,6 +40,7 @@ const RegisterForm = () => {
 
         if (IsUserChek.user) {
           setError("Email Already Exist");
+          toast.error("Email Already Exist")
           setLoading(false)
           return;
         }
@@ -54,12 +57,14 @@ const RegisterForm = () => {
 
         //Complete
         if (response.ok) {
+          toast.success('Your Account has been Created Succesfully')
           // @ts-ignore
           eo.target.reset();
           router.push("/signin");
           setLoading(false)
         } else {
           setError("Failed To Creat Account,Please Try Again");
+          toast.error("Failed To Creat Account,Please Try Again")
           setLoading(false)
         }
 
@@ -122,9 +127,14 @@ const RegisterForm = () => {
         </label>
       </div>
       <button type="submit" className="btn btn-primary">
-        {Loading? "Loading": "Create Account"}
+        {Loading? 
+        <>
+        <span className="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
+        <span style={{marginLeft:"5px"}}>Loading...</span>
+        </>
+        : "Create Account"}
       </button>
-      <p style={{ color: "#ff7790", marginTop: "50px", textAlign: "center" }}>
+      <p style={{ color: "#ff7790", marginTop: "50px", textAlign: "center" ,display: "none" }}>
         {Error}
       </p>
     </form>
